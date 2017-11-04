6fd7c06409f060fd62cc189d9970762c536b203d
Leo <leononame@lho.io>
2017-11-04 16:18:10 -0400

    improved netcat

--- c02/netcat.py
+++ c02/netcat.py
@@ -5,6 +5,7 @@ import socket
 import getopt
 import threading
 import subprocess
+import time
 
 # Globals
 listen = False
@@ -34,14 +35,30 @@ def client_send():
         c.connect((target, port))
 
         while True:
-            buff = raw_input("")
-            while len(buff) <= 0:
-                buff = raw_input("")
-            c.send(buff)
+            # try catch server data first
+            try:
+                c.settimeout(0.5)
+                recv_len = 1
+                resp = ""
+                while recv_len:
+                    data = c.recv(4096)
+                    recv_len = len(data)
+                    resp += data
+                    if recv_len < 4096:
+                        break
+                if len(resp):
+                    print resp,
+            # timeout
+            except:
+                print
 
+            buff = sys.stdin.read()
+
+            c.send(buff + "\n")
+            print buff
+            c.settimeout(2)
             recv_len = 1
             resp = ""
-
             while recv_len:
                 data = c.recv(4096)
                 recv_len = len(data)
@@ -53,15 +70,114 @@ def client_send():
                     break
 
             print resp,
-    except KeyboardInterrupt:
+    # Catch ^c, etc.
+    except (KeyboardInterrupt, EOFError):
         c.close()
         print "[*] Exit..."
+        sys.exit(0)
+    # Catch SystemExit to not show any error
     except SystemExit:
+        c.close()
         sys.exit(0)
     except:
         c.close()
-        print "[*] Exception: ", sys.exc_info()[0]
+        print "[*] Unexpected error: ", sys.exc_info()[0]
         sys.exit(1)
+
+
+def server_run():
+    global target
+
+    # If no target is specified, listen on all ifs
+    if not len(target):
+        target = "0.0.0.0"
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Reuse address
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((target, port))
+    s.listen(5)
+    while True:
+        try:
+            c, addr = s.accept()
+
+            handler = threading.Thread(target=server_handle, args=(c,))
+            # make handler a daemon to stop all threading
+            handler.setDaemon(True)
+            handler.start()
+
+        # Catch ^c, etc.
+        except (KeyboardInterrupt, EOFError):
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            print "[*] Exit..."
+            exit(0)
+        # Catch SystemExit to not show any error
+        except SystemExit:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            sys.exit(0)
+        except:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            print "[*] Unexpected error: ", sys.exc_info()[0]
+            sys.exit(1)
+
+
+# Run a command
+def run_cmd(cmd):
+    # trim
+    cmd = cmd.rstrip()
+    try:
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command.\n"
+
+    return output
+
+
+# Handle a client request
+def server_handle(client):
+    global upld, upld_dest, exe, cmd
+
+    # If file upload
+    if len(upld_dest):
+        # file buffer
+        fbuff = ""
+        # read data into buffer
+        while True:
+            d = client.recv(1024)
+            if not d:
+                break
+            else:
+                fbuff += d
+        # write file and send status
+        try:
+            fd = open(upld_dest, "wb")
+            fd.write(fbuff)
+            fd.close()
+            # ack
+            client.send("Success")
+        except:
+            client.send("Error")
+    # command exec
+    if len(exe):
+        output = run_cmd(exe)
+        client.send(output)
+
+    # ssh
+    if cmd:
+        while True:
+            # send "shell"
+            client.send("root@localhost:/#")
+            # read cmd
+            cmd_buff = ""
+            while "\n" not in cmd_buff:
+                cmd_buff += client.recv(1024)
+            # send response
+            client.send(run_cmd(cmd_buff))
+
+    client.close()
 
 
 def main():
@@ -104,8 +220,7 @@ def main():
         client_send()
     # Listen
     elif listen:
-        #server_run()
-        sys.exit(0)
+        server_run()
 
 
 main()
