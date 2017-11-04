46d8b1c8483a3997aacb4640d5c67cc969ff3a00
Leo <leononame@lho.io>
2017-11-04 12:56:35 -0400

    Add simple UDP client

---
+++ c02/udpclient.py
@@ -0,0 +1,12 @@
+import socket
+
+host = "www.google.com"
+port = 80
+# Create client
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Send data, suppose this worked
+client.sendto("GET / HTTP/1.1\nHost: google.com\n\n", (host, port))
+
+data, addr = client.recvfrom(4096)
+print data
+print addr
