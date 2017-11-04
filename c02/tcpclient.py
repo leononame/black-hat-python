e8c21c975c3ed5329dca83c88e2b1081cc74d868
Leo <leononame@lho.io>
2017-11-04 13:35:47 -0400

    Add simple TCP server

--- c02/tcpclient.py
+++ c02/tcpclient.py
@@ -1,12 +1,12 @@
 import socket
 
-host = "www.google.com"
-port = 80
+host = "127.0.0.1"
+port = 9999
 # Create client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect
 client.connect((host, port))
 # Send data
-client.send("GET / HTTP/1.1\nHost: google.com\n\n")
+client.send("GET / HTTP/1.1\nHost: localhost\n\n")
 response = client.recv(4096)
 print response
---
+++ c02/tcpserver.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+import signal
+import sys
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+# Socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# reuse address
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((bind_ip, bind_port))
+server.listen(5)
+
+
+# # Handler for SIGINT
+# def handle_sigint(signal, frame):
+#     # Close socket
+#     server.close()
+#     sys.exit(0)
+#
+#
+# signal.signal(signal.SIGINT, handle_sigint)
+
+print "[*] Listening on " + bind_ip + ":" + str(bind_port)
+
+
+def handle_client(client_socket):
+    # Get client data
+    req = client_socket.recv(1024)
+
+    print "[*] Received: " + req
+
+    client_socket.send("it works")
+    client_socket.close()
+
+
+while True:
+    try:
+        client, addr = server.accept()
+
+        print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+    except KeyboardInterrupt:
+        client.close()
+        break
+
+print "[*] Closing server..."
+server.close()
